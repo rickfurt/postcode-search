05390b003efaeae3f686225c7659bca9f5b8f8a8
rickfurt <ricardofurtado2609@gmail.com>
2019-10-09 13:17:40 +1100

    fix(src): catch errors

---
+++ src/auspost-api.js
@@ -0,0 +1,24 @@
+const fetch = require("node-fetch");
+require("dotenv").config();
+
+function searchLocalities(postcode, callback) {
+  fetch(
+    `https://digitalapi.auspost.com.au/postcode/search.json?q=${postcode}`,
+    {
+      headers: { "auth-key": process.env.AUTH_KEY }
+    }
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(body) {
+      callback(body.localities.locality);
+    })
+    .catch(() => {
+      callback({
+        success: false
+      });
+    });
+}
+
+exports.searchLocalities = searchLocalities;
--- src/postcode-search.js
+++ src/postcode-search.js
@@ -1,26 +1,32 @@
 function postcodeSearch(dataProvider, postcode, suburb, state, callback) {
-  //validar campos preenchidos.
-  dataProvider.searchLocalities(postcode, suburb, state, function(localities) {
-    let suburbFound = localities.find(function(locality) {
-      return locality.location === suburb;
-    });
-
-    if (suburbFound === undefined) {
+  dataProvider.searchLocalities(postcode,suburb,state, function(localities) {
+    if (localities.success === false) {
       callback({
         success: false,
-        message: `The postcode ${postcode} does not match the suburb ${suburb}.`
+        message: `The postcode ${postcode} does not match the suburb ${suburb} and the state ${state}.`
       });
     } else {
-      if (suburbFound.state === state) {
-        callback({
-          success: true,
-          message: `The postcode, suburb and state entered are valid.`
-        });
-      } else {
+      let suburbFound = localities.find(function(locality) {
+        return locality.location === suburb;
+      });
+
+      if (suburbFound === undefined) {
         callback({
           success: false,
-          message: `The suburb ${suburb} does not exist in the state ${state}.`
+          message: `The postcode ${postcode} does not match the suburb ${suburb}.`
         });
+      } else {
+        if (suburbFound.state === state) {
+          callback({
+            success: true,
+            message: `The postcode, suburb and state entered are valid.`
+          });
+        } else {
+          callback({
+            success: false,
+            message: `The suburb ${suburb} does not exist in the state ${state}.`
+          });
+        }
       }
     }
   });
