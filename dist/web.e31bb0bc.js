7cdc7d756f7985908db0725af21088124172fa45
rickfurt <ricardofurtado2609@gmail.com>
2019-10-28 18:32:36 +1100

    chore(infra): setup Heroku requirements

--- dist/web.e31bb0bc.js
+++ dist/web.e31bb0bc.js
@@ -32259,7 +32259,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49173" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51512" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
